39cd2ed2dd6887823a6dc7ae6d2c2741d1dfae75
Pasechnyktanya <pasechnyktanya@gmail.com>
2023-01-21 17:28:25 +0200

    Homework2

--- Homework2/lesson2.js
+++ Homework2/lesson2.js
@@ -4,81 +4,119 @@ let x = 1;
 let y = 2;
 
 let res1 = String(x) + String(y);
-console.log(res1); 
-console.log(typeof res1); 
+console.log(res1);
+console.log(typeof res1);
 
 let res2 = String(Boolean(x < y)) + String(y);
-console.log(res2); 
-console.log(typeof res2); 
+console.log(res2);
+console.log(typeof res2);
 
-let res3 = x < y; 
-console.log(res3); 
+let res3 = x < y;
+console.log(res3);
 console.log(typeof res3);
 
 let res4 = typeof x / y;
 console.log(res4);
-console.log(typeof res4); 
+console.log(typeof res4);
 
 // task 2
 
 let n = +prompt("Enter your number: ");
-if ( n > 0 && n % 2 === 0) {
-  console.log("Your number is even positive number");
+if (n >= 0 && n % 2 === 0) {
+  console.log("Your number is even positive number.");
 } else {
-  console.log("Your number is not an even positive number");
+  console.log("Your number is not an even positive number.");
 }
 if (n % 7 === 0) {
-  console.log("Your number is a multiple of 7");
+  console.log("Your number is a multiple of 7.");
 } else {
-  console.log("Your number is not a multiple of 7");
+  console.log("Your number is not a multiple of 7.");
 }
 
-/*3. Створіть порожній масив;
-1) У перший елемент масиву запишіть будь-яке число;
-2) У другий елемент масиву запишіть будь-який рядок;
-3) У третій елемент масиву запишіть будь-яке логічне значення;
-4) У четвертий елемент масиву запишіть значення null;
-5) Виведіть на екран число елементів, яке зберігається в масиві;
-6) Запитайте у користувача ввести будь-яке значення і запишіть це значення в п'ятий елемент масиву;
-7) Виведіть на екран п'ятий елемент масиву.
-8) Видаліть 1-ий елемент масиву і виведіть масив на екран.*/
+// task 3
 
 const array = [];
 array[0] = 25;
-array[1] = "string";
+array[1] = "second";
 array[2] = true;
 array[3] = null;
 // console.log(array);
-alert(`Array has ${array.length} elements`);
-array[4] = +prompt("Enter any value: ");
-alert(array[4]);
-array.shift(0);
-alert(array.shift());
-console.log(array);
+alert(`Array has ${array.length} elements.`);
+array[4] = +prompt("Enter any value:", "0");
+alert(`You have entered ${array[4]}.`);
+array.shift();
+alert(array);
+// console.log(array);
 
+// task 4
 
-/*4. Напишіть програму, яка об’єднує всі наявні елементи масиву в стрічку і розділяє їх символом “*”.
-Початковий масив:*/
+let cities = ["Rome", "Lviv", "Warsaw"];
+let newCities = cities.join("*");
+console.log(newCities);
 
-let cities = ["Rome", "Lviv", "Warsaw"]; 
-let newCities = cities.splice("*");
-alert (newCities);
+// task 5
 
-/*Результат:
-"Rome*Lviv*Warsaw"
+let isAdult = +prompt("Are you of legal age? Please, enter your age:", " ");
+if (isAdult >= 18) {
+  alert("You have reached the age of majority.");
+} else {
+  alert("You are still too young.");
+}
 
-5. Використовуючи функцію prompt() задайте користувачу питання про досягнення ним повнолітнього віку. Результат запишіть в змінну isAdult. В залежності від отриманого значення виведіть відповідне повідомлення про статус особи. Наприклад: при виконанні умови вік більше 18 вивести “Ви досягли повнолітнього віку”. Якщо вік менше 18 років вивести “Ви ще надто молоді”, 
+// task 6
 
-6. Користувач вводить три довжини сторін трикутника (використовуйте prompt () три рази для введення кожної сторони).
-Необхідно 
-	a) визначити і вивести в консоль площу трикутника 
-	b) перевірити чи цей трикутник є прямокутним і вивести в консоль результат перевірки.
-Здійснювати перевірку чи введені користувачем значення коректні, в іншому випадку вивести 'Incorrect data‘. Результат обчислення площі трикутника виводити в консоль з точністю 3 знаки після коми (наприклад:  8.42355465 =>  8.424).  
+let a = +prompt("Enter the first side of the triangle:", "");
+let b = +prompt("Enter the second side of the triangle:", "");
+let c = +prompt("Enter the third side of the triangle:", "");
+let p;
+let area;
+if (a < b + c && b < a + c && c < a + b) {
+  p = (a + b + c) / 2;
+  area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+  console.log(`The area of the triangle is ${area.toFixed(3)}`);
+} else {
+  console.log("Incorrect data!!!");
+}
+// - right triangle?
+console.log(
+  a ** 2 === b ** 2 + c ** 2 ||
+    b ** 2 === c ** 2 + a ** 2 ||
+    c ** 2 === a ** 2 + b ** 2
+    ? "It is a right triangle."
+    : "It is not a right triangle."
+);
 
-7. Написати умовну конструкцію, яка в залежності від часу доби виводитиме відповідне привітання. Потрібно отримати реальний час доби із системи. Зробити 2 способами через 2 різних умовних оператора.
-В діапазоні годин 23-5 – має виводитися привітання “Доброї ночі”
-В діапазоні годин 5-11 – має виводитися привітання “Доброго ранку”
-В діапазоні годин 11-17 – має виводитися привітання “Доброго дня”
-В діапазоні годин 17-23 – має виводитися привітання “Доброго вечора”.
+//  7 
 
-*/
+let currentHour = new Date();
+let hour = currentHour.getHours();
+
+// 1 version
+switch (true) {
+  case hour < 5 && hour > 22:
+    alert(" Good night!");
+    break;
+  case hour >= 5 && hour < 11:
+    alert(" Good morning!");
+    break;
+  case hour >= 11 && hour < 17:
+    alert(" Good day!");
+    break;
+  case hour >= 17 && hour < 23:
+    alert(" Good evening");
+    break;
+  default:
+    alert(" Error!");
+}
+// 2 version
+alert(
+  hour < 5 && hour > 22
+    ? " Good night!"
+    : hour >= 5 && hour < 11
+    ? " Good morning!"
+    : hour >= 11 && hour < 17
+    ? " Good day!"
+    : hour >= 17 && hour < 23
+    ? " Good evening"
+    : " Error!"
+);
