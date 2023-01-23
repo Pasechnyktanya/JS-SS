6a1ad250defbecabb072e18949a67c70cc578168
Pasechnyktanya <pasechnyktanya@gmail.com>
2023-01-23 18:35:35 +0200

    Homework1

--- Homework1/lesson1.js
+++ Homework1/lesson1.js
@@ -17,17 +17,17 @@ alert(`${firstname} ${surname}`);
 // task 4
 
 const newObject = {
-  String: "line",
-  Number: 10,
-  Boolean: true,
-  Undefined: undefined,
-  Null: null,
+  customer: "Linda Hotline",
+  card: 316492532410,
+  cash: false,
+  age: undefined,
+  bonus: null,
 };
-console.log(typeof newObject.String);
-console.log(typeof newObject.Number);
-console.log(typeof newObject.Boolean);
-console.log(typeof newObject.Undefined);
-console.log(typeof newObject.Null);
+console.log(typeof newObject.customer);
+console.log(typeof newObject.card);
+console.log(typeof newObject.cash);
+console.log(typeof newObject.age);
+console.log(typeof newObject.bonus);
 
 // task 5
 
@@ -68,16 +68,9 @@ alert(`Dear ${login}, your email is ${email}, your password is ${password}.`);
 //  task 8
 
 const secInHour = 60 ** 2;
-const secInDay = 60 ** 2 * 24; // = sedInHour * 24;
-const secInMounth = 60 ** 2 * 24 * 30; // = secInDay * 30;
+const secInDay = secInHour * 24;
+const secInMounth = secInDay * 30;
 alert(
   `An hour contains ${secInHour} seconds, a day contains ${secInDay} seconds, a month contains ${secInMounth} seconds.`
 );
 
-// or
-const sec_InHour = 60 ** 2;
-alert(`An hour contains ${sec_InHour} seconds.`);
-const sec_InDay = sec_InHour * 24;
-alert(`A day contains ${sec_InDay} seconds.`);
-const sec_InMounth = sec_InDay * 30;
-alert(`A month contains ${sec_InMounth} seconds.`);
