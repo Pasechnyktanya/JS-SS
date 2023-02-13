81ae55b6339b7eb9e7cef444fa97c0d5e7ab2fe9
Pasechnyktanya <pasechnyktanya@gmail.com>
2023-02-13 14:54:04 +0200

    Homework7

--- Homework7/lesson7.js
+++ Homework7/lesson7.js
@@ -1,22 +1,64 @@
 // Practical task
 
-// 1. За допомогою методів об’єкта window створити:
-// 	1) нове вікно розміром 300х300 пікселів.
-// 	2) із затримкою 2 сек змініть розміри вікна на 500х500 пікселів
-// 	3) із затримкою 2 сек перемістіть вікно в точку з координатами (200, 200)
-// 	4) із затримкою 2 сек закрийте вікно.
+//  task 1
+open(
+  "https://www.freecodecamp.org/",
+  "freecodecamp.org/",
+  "width = 300, hieght = 300"
+);
+// в консолі
+// setTimeout(() => {
+//   resizeBy(500, 500);
+// }, 2000);
+// setTimeout(() => {
+//   moveTo(200, 200);
+// }, 2000);
+// setTimeout(() => {
+//   close();
+// }, 2000);
 
-open("https://www.freecodecamp.org/", "freecodecamp.org/", "width = 300, hieght = 300");
-setTimeout(() => {
-    resizeBy(500, 500);
-}, 2000); ;
-setTimeout(() => {
-    moveTo(200, 200);
-}, 2000); ;
-setTimeout(() => {
-    close();
-}, 2000); ;
+//  task 2
+function changeCSS() {
+  document.getElementById("text").style.cssText =
+    "color: orange; font-size: 20px; font-family: Comic Sans MS";
+}
 
+//  task 3
+const changeColorBlue = () => (document.body.style.backgroundColor = "blue");
+document.getElementById("blueBtn").onclick = changeColorBlue;
+ //--
+const changeColorRose = () => (document.body.style.backgroundColor = "hotPink");
+document.getElementById("roseBtn").ondblclick = changeColorRose;
+ //--
+const changeColorBrown = () => (document.body.style.backgroundColor = "brown");
+const changeColorWhite = () => (document.body.style.backgroundColor = "white");
+brownBtn.addEventListener("mouseup", changeColorWhite);
+ //--
+yellowLink.addEventListener("mouseover", (e) => {
+  group.style.backgroundColor = "yellow";
+});
+yellowLink.addEventListener("mouseout", (e) => {
+  group.style.backgroundColor = "white";
+});
 
-
-
+//  task 5
+const pressMes = () =>
+  document
+    .getElementById("livBtn")
+    .insertAdjacentHTML("afterend", "<p>I was pressed!</p>");
+yellowLink.addEventListener("mouseover", (e) => {
+  group.style.backgroundColor = "yellow";
+});
+yellowLink.addEventListener("mouseout", (e) => {
+  group.style.backgroundColor = "white";
+});
+document.getElementById("livBtn").addEventListener("mouseover", (e) => {
+  document
+    .getElementById("livBtn")
+    .insertAdjacentHTML("afterend", "<p>Mouse on me!</p>");
+});
+document.getElementById("livBtn").addEventListener("mouseout", (e) => {
+  document
+    .getElementById("livBtn")
+    .insertAdjacentHTML("afterend", "<p>Mouse is not on me!</p>");
+});
